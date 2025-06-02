6d2b6fbff87f11ce6a2c76e003b7108698d8052c
Jean Milleron <jean.milleron@gmail.com>
2025-06-02 19:58:29 +0200

    Moved definition for env

--- front/src/vite-env.d.ts
+++ front/src/vite-env.d.ts
@@ -1,11 +1,10 @@
-declare global {
-  interface ImportMeta {
-    env: {
-      VITE_BACKEND_URL: string;
-    }
-  }
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {}
