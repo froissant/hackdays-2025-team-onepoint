66199c18abd6b47bc1ef01c54da22d330a36b39a
s.ouidan <s.ouidan@groupeonepoint.com>
2025-06-02 16:31:15 +0200

    add sentiment to system context

--- back-albert/test_albert.py
+++ back-albert/test_albert.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import json
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
+import vars
 
     
 app = FastAPI()
@@ -54,7 +55,7 @@ tools = [
 
 client = OpenAI(
     base_url = 'https://albert.api.etalab.gouv.fr/v1',
-    api_key='{{ALBERT_API_KEY}}',
+    api_key=vars.ALBERT_API_KEY,
 )
 
 def getParamsFromPrompt(prompt: str):
@@ -62,7 +63,8 @@ def getParamsFromPrompt(prompt: str):
         model="albert-small",
         messages=[
             {"role": "system", "content": "You are memeGenAi, you will be prompted to generate memes and you are required to extract the id of the meme template, the upper text and the lower text."
-            "the id of the template must absolutely exist in the json array below that contains the id and name of many meme templates. It is your task to know which template the user is refering to, by comparing the description to the names and ids, and return the corresponding id."
+            "the id of the template must absolutely exist in the json array below that contains the id, name, sentiment and useCase of many meme templates. It is your task to know which template the user is refering to, by comparing the description to the names and ids, and return the corresponding id."
+            "If the user does not refer to a specific template but rather describes a feeling or sentiment, you can refer to the fields sentiments and useCase to make a convenient choice and return the id."
             "For example, if the user says 'I want a meme of the crazy alien dude' it is your task to know they're refering to the ancient aliens guy, so you return 'aag' as the id of the meme template."
             "Here is the Json of all the memes:\n\n" + templates},
             {"role": "user", "content": prompt}
