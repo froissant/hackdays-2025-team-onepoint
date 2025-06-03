b6bd0d90a92bfacfb028ed0eea9a12349d353c07
Anthony Froissant <froissant@users.noreply.github.com>
2025-06-03 11:15:21 +0200

    🐋 Add back Dockerfile

--- back/index.ts
+++ back/index.ts
@@ -6,24 +6,25 @@ import 'dotenv/config'
 import MemeGenerationController from './controllers/MemeGenerationController'
 import TLDrawSyncController from './controllers/TLDrawSyncController'
 
-const PORT = process.env.PORT;
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
 
 // For this example we use a simple fastify server with the official websocket plugin
 // To keep things simple we're skipping normal production concerns like rate limiting and input validation.
-const app = fastify({ logger: true })
-app.register(websocketPlugin)
-app.register(cors, { origin: '*', methods: ["POST", "GET", "PUT", "PATCH"] })
+const app = fastify({ logger: true });
+app.register(websocketPlugin);
+app.register(cors, { origin: '*', methods: ["POST", "GET", "PUT", "PATCH"] });
 
 app.register(MemeGenerationController, { prefix: '/memes' });
-app.register(TLDrawSyncController, { prefix: '/sync' })
+app.register(TLDrawSyncController, { prefix: '/sync' });
 
 app.get('/ping', async (req, res) => {
-  res.send("pong")
-})
+	res.send("pong")
+});
 
-app.listen({ port: PORT }, (err, address) => {
+app.listen({ host: HOST, port: PORT }, (err, address) => {
 	if (err) {
 		console.error(err)
 		process.exit(1)
 	}
-})
+});
